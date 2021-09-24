b2847d1dbd95d784305dd3a46a6b195b426ec641
Nikhita Thomas <nikhitathomas@Nikhitas-MacBook-Air.local>
2021-09-24 19:07:27 +0530

    uploading files

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import './Header.css'
+
+const Header = ({ content }) => {
+    return (
+        <div className="header-content">
+            {content}
+        </div>
+    )
+}
+
+export default Header
